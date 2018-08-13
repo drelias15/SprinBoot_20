55c5cafd0efef279c2e8fd84782dbca05cb1b225
drelias15 <drelias15@gmail.com>
2018-08-13 18:01:16 -0400

    encoder

--- src/main/java/com/example/springboot_20/UserService.java
+++ src/main/java/com/example/springboot_20/UserService.java
@@ -1,12 +1,17 @@
 package com.example.springboot_20;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 import java.util.Arrays;
 
 @Service
 public class UserService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     UserRepository userRepository;
 
@@ -30,10 +35,12 @@ public class UserService {
     public void saveUser(User user){
         user.setRoles(Arrays.asList(roleRepository.findByRole("USER")));
         user.setEnabled(true);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
     public void saveAdmin(User user){
         user.setRoles(Arrays.asList(roleRepository.findByRole("ADMIN")));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setEnabled(true);
         userRepository.save(user);
     }
