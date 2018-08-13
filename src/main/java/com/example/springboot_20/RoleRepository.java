0536a8ea21034e485a22e1c07a52e44ddbc536b7
drelias15 <drelias15@gmail.com>
2018-08-13 17:39:23 -0400

    lesson 21

--- src/main/java/com/example/springboot_20/RoleRepository.java
+++ src/main/java/com/example/springboot_20/RoleRepository.java
@@ -1,6 +1,10 @@
 package com.example.springboot_20;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface RoleRepository extends CrudRepository<User, Long> {
+
+    Role findByRole(String role);
 }
--- src/main/java/com/example/springboot_20/UserRepository.java
+++ src/main/java/com/example/springboot_20/UserRepository.java
@@ -1,7 +1,14 @@
 package com.example.springboot_20;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+    User findByEmail(String email);
+    Long countByEmail(String email);
+    Long countByUsername(String username);
+
+
 }
