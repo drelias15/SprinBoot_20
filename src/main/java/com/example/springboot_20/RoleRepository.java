f629ec7a28dd787df573d6f426954bddd82c4036
drelias15 <drelias15@gmail.com>
2018-08-14 10:49:55 -0400

    final

--- src/main/java/com/example/springboot_20/RoleRepository.java
+++ src/main/java/com/example/springboot_20/RoleRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface RoleRepository extends CrudRepository<User, Long> {
+public interface RoleRepository extends CrudRepository<Role, Long> {
 
     Role findByRole(String role);
 }
